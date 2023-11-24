65070870a3d4498108b272f818c18c0d4ce4526b
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2023-11-24 13:02:52 +0100

    Add Doctrine Fixtures & Faker Library

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\Personne;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PersonneController extends AbstractController
 {
-    #[Route('/personne', name: 'app_personne')]
-    public function index(): Response
+    #[Route('/personne/add', name: 'parsonne.add')]
+    public function addPersonne(ManagerRegistry $doctrine): Response
     {
-        return $this->render('personne/index.html.twig', [
-            'controller_name' => 'PersonneController',
-        ]);
+        // $this->getDoctrine : Sf <= 5
+        $entityManager = $doctrine->getManager();
+
+        $personne = new Personne();
+        $personne->setFirstname('Michael');
+        $personne->setName('CHLON');
+        $personne->setAge('50');
+
+        // $personne2 = new Personne();
+        // $personne2->setFirstname('Toto');
+        // $personne2->setName('CHLON');
+        // $personne2->setAge('5');
+
+        // Ajouter l'pération de la personne dans la transaction
+        $entityManager->persist($personne);
+        // $entityManager->persist($personne2);
+
+        // Execute la transaction
+        $entityManager->flush();
+
+        return $this->render(
+            'personne/detail.html.twig', [
+            'personne' => $personne
+            ]
+        );
     }
 }
